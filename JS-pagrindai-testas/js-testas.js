1a346b5c65eb4f41d7d2244ef26c77dd8dd94117
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2024-12-30 16:15:56 +0000

    first commit

---
+++ JS-pagrindai-testas/js-testas.js
@@ -0,0 +1,68 @@
+/*
+1. Sukurkite funkciją, kuri paverstų eurus į dolerius.
+Sugeneruokite atsitiktinį skaičių (eurų sumą) nuo 1 iki 1000.
+Pritaikykite savo sukurtą funkciją ir išspausdinkite atitikmenį
+doleriais.
+*/
+
+/*
+2. Sukurkite funkciją, kuri paverstų dolerius į eurus.
+Sugeneruokite atsitiktinį skaičių (dolerių sumą) nuo 1 iki 1000.
+Pritaikykite savo sukurtą funkciją ir išspausdinkite atitikmenį
+eurais.
+*/
+
+/*
+3. Parašykite programą, kuri suskaičiuotų žmogaus BMI (body
+mass index), kai yra žinomas žmogaus ūgis ir svoris. 
+BMI formulė: žmogaus svoris (kg) padalintas iš ūgio metrais kvadratu. 
+Pvz. svoris = 80kg, ūgis = 1.8 m. 
+BMI = 80 / 1.8**2 = 24.69
+Viršsvoris = BMI > 25
+Normalu = 18.5 <= BMI < 25
+Per mažas svoris = BMI < 18.5
+*/
+
+/*
+4. Parašykite programą, kuri iš duoto žmogaus amžiaus metais
+pasakytų kiek tai yra sekundėmis, minutėmis, valandomis,
+dienomis.
+*/
+
+/*
+5. Parašykite programą, kuri konvertuos termometro
+duomenis iš Farenheito į Celsijų, ir atvirkščiai.
+*/
+
+/*
+6. Sukurkite kodą, kuris išspausdins į konsolę
+1-2-3-4-5-6-7-8-9-10 vienoje eilutėje. Prieš vienetą ir po
+dešimties neturėtų būti brūkšniuko.
+*/
+
+/*
+7. Panaudokite for ciklus, kad sukurtumėte tokį vaizdą konsolėje.
+*
+**
+***
+****
+*****
+*/
+
+/*
+8. Parašykite kodą, kuris apskaičiuos kiek liko dienų iki Kalėdų.
+*/
+
+/*
+9. Parašykite kodą, kuris apjungia masyvo duomenis į vieną
+tekstinę eilutę. Turėtumėte gauti tokį rezultatą:
+Tomas,Dainius,Paulius,Jonas
+Tomas+Dainius+Paulius+Jonas
+*/
+
+/*
+10. Parašykite kodą, kuris sugeneruos dvylikos simbolių
+slaptažodį. Slaptažodyje privalo būti bent po vieną: didžioji raidė,
+mažoji raidė, skaičius, specialusis simbolis. Visi slaptažodžio
+simboliai privalo būti atsitiktiniai ir atsitiktine tvarka.
+*/
